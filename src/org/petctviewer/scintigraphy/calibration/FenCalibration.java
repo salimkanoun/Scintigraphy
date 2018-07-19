92ec6436fbd3716c63354355f780d7b6139a067f
Salim Kanoun <bastienreturn@outlook.fr>
2018-07-19 14:35:44 +0200

    erreur

--- src/org/petctviewer/scintigraphy/calibration/FenCalibration.java
+++ src/org/petctviewer/scintigraphy/calibration/FenCalibration.java
@@ -2,7 +2,6 @@ package org.petctviewer.scintigraphy.calibration;
 
 import javax.swing.JFrame;
 
-import org.petctviewer.scintigraphy.CSV.FenApplication_FollowUp;
 import org.petctviewer.scintigraphy.scin.Scintigraphy;
 import org.petctviewer.scintigraphy.scin.gui.FenApplication;
 import org.petctviewer.scintigraphy.scin.gui.FenSelectionDicom;
