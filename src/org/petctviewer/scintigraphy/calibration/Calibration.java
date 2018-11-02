d9b84ba4d8a5b0cea7dd86b079c27e6aff3043a4
Salim Kanoun <salim.kanoun@gmail.com>
2018-11-02 14:55:49 +0100

    ref

--- src/org/petctviewer/scintigraphy/calibration/Calibration.java
+++ src/org/petctviewer/scintigraphy/calibration/Calibration.java
@@ -8,13 +8,6 @@ public class Calibration implements PlugIn{
 
 	@Override
 	public void run(String arg0) {
-		/*
-		try {
-		    UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
-		 } catch (Exception e) {
-		            e.printStackTrace();
-		 }
-		*/
 		FenChargementCalibration fen = new FenChargementCalibration();
 		fen.setVisible(true);
 	}
