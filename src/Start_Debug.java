7d9bee633e5a49cfb4a48a930cdf85708f99fa39
Salim Kanoun <salim.kanoun@gmail.com>
2019-04-08 17:40:34 +0200

    debug

--- src/Start_Debug.java
+++ src/Start_Debug.java
@@ -1,3 +1,5 @@
+import org.petctviewer.petcttools.reader.Dicom_Reader;
+
 import ij.ImageJ;
 
 public class Start_Debug {
@@ -5,7 +7,7 @@ public class Start_Debug {
 	public static void main(String[] args) {
 		new ImageJ();
 
-		Read_CD cd = new Read_CD();
+		Dicom_Reader cd = new Dicom_Reader();
 		cd.run("");
 		
 		new FenDebug();
