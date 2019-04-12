c04588b387ce4e201c772069629c374d2bfda9e5
Noa <noa.fayn@gmail.com>
2019-04-12 19:22:23 +0200

    Work In Progress
    Work on the model

--- src/org/petctviewer/scintigraphy/shunpo/ModeleShunpo.java
+++ src/org/petctviewer/scintigraphy/shunpo/ModeleShunpo.java
@@ -7,121 +7,75 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.petctviewer.scintigraphy.scin.ModeleScin;
+import org.petctviewer.scintigraphy.scin.library.Library_Quantif;
 
-import ij.IJ;
 import ij.ImagePlus;
-import ij.process.ImageStatistics;
 
 public class ModeleShunpo extends ModeleScin {
-	
-	private Map<String, Double> coups;
+
+	private Map<Integer, Double> coups;
 	private int pixrdp;
 	private int pixrgp;
 	private int pixrda;
 	private int pixrga;
-	private HashMap<String, Integer> mgs;
+	private HashMap<Integer, Integer> geometricalAverage;
 	private String[] abvsMG = { "PD", "PG", "RD", "RG", "C" };
 	private String[] retour;
-	
-	protected void calculerCoups(String roi, ImagePlus imp) {
-		ImageStatistics is = imp.getStatistics();
 
-		if (roi.contains("BDF"))
-			coups.put(roi, is.mean);
-		else {
-			if (roi.contains("R")) {
-				if (roi.equals("RDP"))
-					pixrdp = is.pixelCount;
-				if (roi.equals("RGP"))
-					pixrgp = is.pixelCount;
-				if (roi.equals("RDA"))
-					pixrda = is.pixelCount;
-				if (roi.equals("RGA"))
-					pixrga = is.pixelCount;
-			}
-			coups.put(roi, is.pixelCount * is.mean);
-		}
-		if (Controleur_Shunpo.showLog) {
-			IJ.log(roi + "coups= " + String.valueOf(is.pixelCount * is.mean));
-		}
+	public static final int PULMON_RIGHT_ANT = 0, PULMON_RIGHT_POST = 1, PULMON_LEFT_ANT = 2, PULMON_LEFT_POST = 3,
+			KIDNEY_RIGHT_ANT = 4, KIDNEY_RIGHT_POST = 5, KIDNEY_LEFT_ANT = 6, KIDNEY_LEFT_POST = 7, BRAIN_ANT = 8,
+			BRAIN_POST = 9, BACKGROUND_NOISE_ANT = 10, BACKGROUND_NOISE_POST = 11, TOTAL_ORGANS = 12;
+
+	protected void calculerCoups(int organ, ImagePlus imp) {
+		this.coups.put(organ, Library_Quantif.getCounts(imp));
 	}
 
 	// Retrait du BDF aux reins
 	private void coupsReins() {
-		double rdp = coups.get("RDP");
-		double rgp = coups.get("RGP");
-		double bdfp = coups.get("BDFP");
-		coups.put("RDP", rdp - (bdfp * pixrdp));
-		coups.put("RGP", rgp - (bdfp * pixrgp));
-		double rda = coups.get("RDA");
-		double rga = coups.get("RGA");
-		double bdfa = coups.get("BDFA");
-		coups.put("RDA", rda - (bdfa * pixrda));
-		coups.put("RGA", rga - (bdfa * pixrga));
+		double rdp = coups.get(KIDNEY_RIGHT_POST);
+		double rgp = coups.get(KIDNEY_LEFT_POST);
+		double bdfp = coups.get(BACKGROUND_NOISE_POST);
+		coups.put(KIDNEY_RIGHT_POST, rdp - (bdfp * pixrdp));
+		coups.put(KIDNEY_LEFT_POST, rgp - (bdfp * pixrgp));
+		double rda = coups.get(KIDNEY_RIGHT_ANT);
+		double rga = coups.get(KIDNEY_LEFT_ANT);
+		double bdfa = coups.get(BACKGROUND_NOISE_ANT);
+		coups.put(KIDNEY_RIGHT_ANT, rda - (bdfa * pixrda));
+		coups.put(KIDNEY_LEFT_ANT, rga - (bdfa * pixrga));
 	}
 
-	private void mgs() {
-		for (String abv : abvsMG)
-			moyenneGeo(abv);
+	private void computeGeometricalAverage() {
+		for (int i = 0; i < TOTAL_ORGANS; i += 2)
+			this.moyenneGeo(i);
 	}
 
 	// Calcule la moyenne g茅om茅trique pour un organe sp茅cifique
 	// Si abv = PD alors on calculera la MG pour le poumon droit
-	private void moyenneGeo(String abv) {
-		double[] coupsa = new double[2];
-		String[] asuppr = new String[2];
-		int index = 0;
-		for (Entry<String, Double> entry : coups.entrySet()) {
-			if (entry.getKey().contains(abv)) {
-				coupsa[index] = entry.getValue();
-				asuppr[index] = entry.getKey();
-				index++;
-			}
-		}
-		for (String so : asuppr)
-			coups.remove(so);
-		mgs.put("MG" + abv, moyenneGeometrique(coupsa));
+	private void moyenneGeo(int organ) {
+		geometricalAverage.put(organ, (int) Library_Quantif.moyGeom(this.coups.get(organ), this.coups.get(organ + 1)));
 	}
 
-	// Calcule la moyenne g茅om茅trique des nombres en param猫tre
-	private int moyenneGeometrique(double[] vals) {
-		double result = 1.0;
-		for (int i = 0; i < vals.length; i++) {
-			result *= vals[i];
+	private String convertOrgan(int organ) {
+		switch (organ) {
+		case PULMON_RIGHT_ANT:
+		case PULMON_RIGHT_POST:
+			return "Right Pulmon: ";
+		case PULMON_LEFT_ANT:
+		case PULMON_LEFT_POST:
+			return "Left Pulmon: ";
+		case KIDNEY_RIGHT_ANT:
+		case KIDNEY_RIGHT_POST:
+			return "Right Kidney: ";
+		case BRAIN_ANT:
+		case BRAIN_POST:
+			return "Brain: ";
+		default:
+			return "Unknown Organ: ";
 		}
-		result = Math.sqrt(result);
-		return (int) result;
 	}
 
-	private String convertAbrev(String abv) {
-		char[] decomp = abv.toCharArray();
-		String result = "";
-		for (int i = 0; i < decomp.length; i++) {
-			switch (decomp[i]) {
-			case 'P':
-				result += "Poumon ";
-				break;
-			case 'R':
-				result += "Rein ";
-				break;
-			case 'C':
-				result += "Cerveau ";
-				break;
-			case 'M':
-				result += "MG ";
-				i++;
-				break;
-			case 'D':
-				result += "Droite ";
-				break;
-			case 'G':
-				result += "Gauche ";
-				break;
-			}
-			if (i == decomp.length - 1)
-				result += ": ";
-		}
-		return result;
+	public String[] getResult() {
+		return this.retour;
 	}
 
 	@Override
@@ -130,9 +84,9 @@ public class ModeleShunpo extends ModeleScin {
 		coupsReins();
 		int index = 0;
 		// Les 5 MGs
-		mgs();
-		for (Entry<String, Integer> entry : mgs.entrySet()) {
-			retour[index] = convertAbrev(entry.getKey()) + entry.getValue();
+		computeGeometricalAverage();
+		for (Entry<Integer, Integer> entry : geometricalAverage.entrySet()) {
+			retour[index] = convertOrgan(entry.getKey()) + entry.getValue();
 			index++;
 		}
 		// Permet de definir le nombre de chiffre apr猫s la virgule et mettre la
@@ -142,14 +96,17 @@ public class ModeleShunpo extends ModeleScin {
 		DecimalFormat us = new DecimalFormat("##.##");
 		us.setDecimalFormatSymbols(sym);
 		// Calculs
-		double percPD = (mgs.get("MGPD") / (1.0 * mgs.get("MGPD") + mgs.get("MGPG"))) * 100;
+		double percPD = (geometricalAverage.get(PULMON_RIGHT_ANT)
+				/ (1.0 * geometricalAverage.get(PULMON_RIGHT_ANT) + geometricalAverage.get(PULMON_LEFT_ANT))) * 100;
 		retour[3] += " (" + us.format(percPD) + "%)";
-		double percPG = (mgs.get("MGPG") / (1.0 * mgs.get("MGPD") + mgs.get("MGPG"))) * 100;
+		double percPG = (geometricalAverage.get(PULMON_LEFT_ANT)
+				/ (1.0 * geometricalAverage.get(PULMON_RIGHT_ANT) + geometricalAverage.get(PULMON_LEFT_ANT))) * 100;
 		retour[0] += " (" + us.format(percPG) + "%)";
-		int totmg = mgs.get("MGPD") + mgs.get("MGPG");
+		int totmg = geometricalAverage.get(PULMON_RIGHT_ANT) + geometricalAverage.get(PULMON_LEFT_ANT);
 		retour[index] = "Total MG : " + totmg;
 		index++;
-		int totshunt = mgs.get("MGRD") + mgs.get("MGRG") + mgs.get("MGC");
+		int totshunt = geometricalAverage.get(KIDNEY_RIGHT_ANT) + geometricalAverage.get(KIDNEY_LEFT_ANT)
+				+ geometricalAverage.get(BRAIN_ANT);
 		retour[index] = "Total Shunt : " + totshunt;
 		index++;
 		double percSyst = (100.0 * totshunt) / totmg;
