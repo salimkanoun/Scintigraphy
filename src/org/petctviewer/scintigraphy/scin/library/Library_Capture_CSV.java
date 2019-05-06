e2fb239c6b4a069beec929fbd2ee8eae6efda3d8
Noa <noa.fayn@gmail.com>
2019-05-06 14:06:41 +0200

    Small change on Library CSV
    Removed method captureToStack for ImageSelection

--- src/org/petctviewer/scintigraphy/scin/library/Library_Capture_CSV.java
+++ src/org/petctviewer/scintigraphy/scin/library/Library_Capture_CSV.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Random;
 
-import org.petctviewer.scintigraphy.scin.ImageSelection;
 import org.petctviewer.scintigraphy.scin.ModeleScin;
 
 import ij.IJ;
@@ -146,36 +145,13 @@ public class Library_Capture_CSV {
 	}
 
 	/**
-	 * Creates a stack from an array of images.<br>
-	 * <b><i>Important</b></i>: all images in the array <b>must</b> have the same
-	 * dimension.
-	 * 
-	 * @param captures Array containing the images to place in the stack.
-	 * @return stack of images
-	 */
-	public static ImageStack captureToStack(ImageSelection[] captures) {
-		// On cree de stack de taille adhoc
-		int[] dimensions = captures[0].getImagePlus().getDimensions();
-		ImageStack stackCapture = new ImageStack(dimensions[0], dimensions[1]);
-		// On rajoute les images dans le stack
-		for (int i = 0; i < captures.length; i++) {
-
-			stackCapture.addSlice(captures[i].getImagePlus().getProcessor());
-		}
-		// On retourne le stack de Capture
-		return stackCapture;
-	}
-
-	/**
 	 * Permet de creer un stack a partir d'un tableau d'ImagePlus *
 	 * 
 	 * @param tableauImagePlus : Tableau contenant les ImagePlus a mettre dans le
 	 *                         stack (toutes les images doivent avoir la m�me
 	 *                         taille)
 	 * @return Renvoie le stack d'image produit
-	 * @deprecated Please use {@link #captureToStack(ImageSelection[])} instead
 	 */
-	@Deprecated
 	public static ImageStack captureToStack(ImagePlus[] tableauImagePlus) {
 		// On verifie que toutes les images ont la meme taille
 		int[][] dimensionCapture = new int[tableauImagePlus.length][2];
