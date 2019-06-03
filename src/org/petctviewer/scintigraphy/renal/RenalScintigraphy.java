ad96c54e714331d7c780ac5962d7626339252ea1
Noa <noa.fayn@gmail.com>
2019-06-03 09:50:34 +0200

    Fixed image input of renal

--- src/org/petctviewer/scintigraphy/renal/RenalScintigraphy.java
+++ src/org/petctviewer/scintigraphy/renal/RenalScintigraphy.java
@@ -36,14 +36,18 @@ public class RenalScintigraphy extends Scintigraphy {
 		// Check orientations
 		// With 1 image
 		if (selectedImages.length == 1) {
-			if (selectedImages[0].getImageOrientation() != Orientation.DYNAMIC_ANT_POST)
+			if (selectedImages[0].getImageOrientation() == Orientation.DYNAMIC_ANT_POST) {
+				// Set images
+				ImageSelection[] imps = Library_Dicom.splitDynamicAntPost(selectedImages[0]);
+				this.impAnt = imps[0];
+				this.impPost = imps[1];
+			} else if (selectedImages[0].getImageOrientation() == Orientation.DYNAMIC_POST) {
+				// Only Dyn Post
+				this.impPost = selectedImages[0];
+			} else
 				throw new WrongColumnException.OrientationColumn(selectedImages[0].getRow(),
-						selectedImages[0].getImageOrientation(), new Orientation[]{Orientation.DYNAMIC_ANT_POST}, "You"
-						+ " can also use 2 dynamics (Ant and Post)");
-			// Set images
-			ImageSelection[] imps = Library_Dicom.splitDynamicAntPost(selectedImages[0]);
-			this.impAnt = imps[0];
-			this.impPost = imps[1];
+						selectedImages[0].getImageOrientation(), new Orientation[]{Orientation.DYNAMIC_POST,
+						Orientation.DYNAMIC_ANT_POST}, "You" + " can also use 2 dynamics (Ant and Post)");
 		}
 		// With 2 images
 		else {
@@ -71,18 +75,23 @@ public class RenalScintigraphy extends Scintigraphy {
 		}
 
 		// Close images
-		for(ImageSelection ims : selectedImages)
+		for (ImageSelection ims : selectedImages)
 			ims.getImagePlus().close();
 
-		// Flip Ant
-		for (int i = 1; i <= this.impAnt.getImagePlus().getStackSize(); i++) {
-			this.impAnt.getImagePlus().getStack().getProcessor(i).flipHorizontal();
-		}
-
 		// Build frame duration
-		this.frameDurations = Library_Dicom.buildFrameDurations(this.impAnt.getImagePlus());
-		if (!ArrayUtils.isEquals(this.frameDurations, Library_Dicom.buildFrameDurations(this.impPost.getImagePlus())))
-			throw new WrongInputException("Frame durations are not the same for Ant and Post!");
+		this.frameDurations = Library_Dicom.buildFrameDurations(this.impPost.getImagePlus());
+
+		// Ant processing
+		if (this.impAnt != null) {
+			// Check frame duration identical
+			if (!ArrayUtils.isEquals(this.frameDurations, Library_Dicom.buildFrameDurations(this.impAnt.getImagePlus())))
+				throw new WrongInputException("Frame durations are not the same for Ant and Post!");
+
+			// Flip Ant
+			for (int i = 1; i <= this.impAnt.getImagePlus().getStackSize(); i++) {
+				this.impAnt.getImagePlus().getStack().getProcessor(i).flipHorizontal();
+			}
+		}
 
 		// TODO: from this part, still no refactored @noa
 		// \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/
