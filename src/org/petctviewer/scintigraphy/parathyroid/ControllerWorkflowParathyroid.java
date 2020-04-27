5e2b74ba4be27b01ac08884921e8e1594a799b13
Angèle MATEOS <angele_m@orange.fr>
2020-04-27 11:54:13 +0200

    correction des modifs d'affichage

--- src/org/petctviewer/scintigraphy/parathyroid/ControllerWorkflowParathyroid.java
+++ src/org/petctviewer/scintigraphy/parathyroid/ControllerWorkflowParathyroid.java
@@ -1,7 +1,9 @@
 package org.petctviewer.scintigraphy.parathyroid;
 
+import org.petctviewer.scintigraphy.scin.Orientation;
 import org.petctviewer.scintigraphy.scin.controller.ControllerWorkflow;
 import org.petctviewer.scintigraphy.scin.gui.FenApplicationWorkflow;
+import org.petctviewer.scintigraphy.scin.instructions.ImageState;
 import org.petctviewer.scintigraphy.scin.model.ModelScin;
 
 import java.awt.event.ItemEvent;
@@ -10,6 +12,7 @@ import java.util.Arrays;
 
 public class ControllerWorkflowParathyroid extends ControllerWorkflow implements ItemListener {
 
+	private static final int SLICE_ANT = 1;
 	private DisplayState display;
 
     public ControllerWorkflowParathyroid(FenApplicationWorkflow vue, ModelScin model) {
@@ -18,7 +21,37 @@ public class ControllerWorkflowParathyroid extends ControllerWorkflow implements
 
 		this.generateInstructions();
 		this.start();
-	    }
+	}
+
+	public ModelParathyroid getModel() {
+		return (ModelParathyroid) super.getModel();
+	}
+
+	// TODO: remove this method and do this in the instructions
+	private void computeModel() {
+		ImageState stateAnt = new ImageState(Orientation.ANT, SLICE_ANT, ImageState.LAT_RL, ModelParathyroid.IMAGE_THYROID);
+		final int NB_ROI_PER_IMAGE = 1;
+		// Just Ant
+		for (int i = 0; i < 2; i++) {
+			ImageState state;
+			state = stateAnt;
+			// - Thyroid Only
+			getModel().addData(ModelParathyroid.REGION_THYRO, state,
+							   getRoiManager().getRoisAsArray()[NB_ROI_PER_IMAGE * i]);
+			// - Thyroid and parathyroid
+			getModel().addData(ModelParathyroid.REGION_THYRO_PARA, state,
+							   getRoiManager().getRoisAsArray()[NB_ROI_PER_IMAGE * i + 1]);
+		}
+
+		// - Brain Post
+		statePost.setIdImage(ModelShunpo.IMAGE_BRAIN);
+		getModel().addData(ModelShunpo.REGION_BRAIN, statePost,
+						   getRoiManager().getRoisAsArray()[NB_ROI_PER_IMAGE * 2]);
+		// - Brain Ant
+		stateAnt.setIdImage(ModelShunpo.IMAGE_BRAIN);
+		getModel().addData(ModelShunpo.REGION_BRAIN, stateAnt,
+						   getRoiManager().getRoisAsArray()[NB_ROI_PER_IMAGE * 2 + 1]);
+	}
 
     @Override
     public void itemStateChanged(ItemEvent e) {
