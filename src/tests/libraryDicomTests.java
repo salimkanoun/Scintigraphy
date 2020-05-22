334c1a1a180afca48ff17b92e2bf9d0ab236361a
diegorodriguez31 <diego.guilhem65@gmail.com>
2020-05-22 16:44:28 +0200

    try to mock

--- src/tests/libraryDicomTests.java
+++ src/tests/libraryDicomTests.java
@@ -1,6 +1,12 @@
 package tests;
 
+import ij.ImagePlus;
 import ij.plugin.DICOM;
+import ij.process.StackProcessor;
+import ij.util.DicomTools;
+import loci.plugins.util.LibraryChecker.Library;
+import org.easymock.EasyMock;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,7 +20,11 @@ import java.util.Date;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.petctviewer.scintigraphy.scin.ImageSelection;
+import org.petctviewer.scintigraphy.scin.Orientation;
 import org.petctviewer.scintigraphy.scin.exceptions.ReadTagException;
+import org.petctviewer.scintigraphy.scin.exceptions.WrongOrientationException;
+import org.petctviewer.scintigraphy.scin.instructions.ImageState;
 import org.petctviewer.scintigraphy.scin.library.Library_Dicom;
 
 /**
@@ -28,15 +38,16 @@ public class libraryDicomTests {
     private DICOM dcm;
     private DICOM dcm2;
     private String str = "Images/testImage.dcm";
-    private String str2 = "Images/testImage2.dcm";
+    private String str2 = "Images/poumonsReins.dcm";
     private InputStream is;
     private InputStream is2;
     private BufferedInputStream bis;
     private BufferedInputStream bis2;
 
     public static void main(String[] args) {
-        libraryQuantifTests lib = new libraryQuantifTests();
-        lib.setUp();
+
+        libraryDicomTests dic = new libraryDicomTests();
+        dic.setUp();
     }
 
     @BeforeEach
@@ -50,7 +61,7 @@ public class libraryDicomTests {
         this.is2 = this.getClass().getResourceAsStream(this.str2);
         this.bis2 = new BufferedInputStream(this.is2);
         this.dcm2 = new DICOM(this.bis2);
-        this.dcm2.run("Test image2");
+        this.dcm2.run("Test image2");        
         this.dcm2.show();
     }
 
@@ -82,13 +93,76 @@ public class libraryDicomTests {
     }
 
     @Test
-    public void isMultiFrameTest(){
+    public void isMultiFrameTest() {
         boolean res = Library_Dicom.isMultiFrame(this.dcm);
         assertFalse(res);
     }
 
+    //TODO : pas testable ?
+   /* @Test
+    public void splitDynamicAntPostTest() throws WrongOrientationException, IllegalArgumentException, ReadTagException {
+        ImageState state = new ImageState(Orientation.ANT, 0, true, 0);   
+        ImageSelection ims = state.getImage();
+        ims.setImagePlus(this.dcm);  
+        ImageSelection[] imsT = Library_Dicom.splitDynamicAntPost(ims);
+        assertEquals(4, imsT);
+    }*/
+/*
     @Test
-    public void splitDynamicAntPostTest(){
-       // ImageSelection[] ims = Library_Dicom.splitDynamicAntPost(this.dcm);
+    public void sortImageAntPostTest() throws ReadTagException {
+        ImagePlus imp = Library_Dicom.sortImageAntPost(this.dcm2);
+        String tag = DicomTools.getTag(imp, "0011,1012");
+        System.out.println(tag);
+        System.out.println(DicomTools.getTag(this.dcm2, "0011,1012"));
+        assertEquals(this.dcm2, imp);
+    }*/
+
+    @Test
+    public void getFrameDurationTest(){
+        int res = Library_Dicom.getFrameDuration(this.dcm);
+        assertEquals(167961, res);
+    }
+
+    //TODO : pas testable ?
+    /*@Test
+    public void flipStackHorizontalTest(){
+        String[] str1 = {"un"};
+        String[] str2 = {"deux"};
+        ImageSelection ims = new ImageSelection(this.dcm, str1, str2);
+        StackProcessor sp = new StackProcessor(ims.getImagePlus().getImageStack());
+        Library_Dicom.flipStackHorizontal(ims);
+        StackProcessor sp2 = new StackProcessor(ims.getImagePlus().getImageStack());
+        assertEquals(sp.toString(), sp2.toString());
+    }*/
+
+    @Test
+    public void buildFrameDurationsTest(){
+        int[] res = Library_Dicom.buildFrameDurations(this.dcm);
+        int[] waited = {Library_Dicom.getFrameDuration(this.dcm)};
+        assertEquals(waited[0], res[0]);
+    }
+
+    @Test
+    public void projectTest(){
+        String[] str1 = {"un"};
+        String[] str2 = {"deux"};
+        ImageSelection ims = new ImageSelection(this.dcm, str1, str2);
+        ImageSelection res = Library_Dicom.project(ims, 1, 2 ,"Max Intensity");
+        assertEquals("MAX_Test image", res.getImagePlus().getTitle());
+    }
+
+    @Test
+    public void ensureAntPostTest() throws WrongOrientationException {
+
+       // String[] str1 = {"un"};
+       // String[] str2 = {"deux"};
+        
+        ImageSelection mockImageSelection = EasyMock.createMock(ImageSelection.class);
+        EasyMock.expect(mockImageSelection.getImageOrientation()).andReturn(Orientation.POST_ANT);
+        EasyMock.replay(mockImageSelection);
+        //ImageSelection ims = new ImageSelection(this.dcm2, str1, str2);  
+        ImageSelection res = Library_Dicom.ensureAntPost(mockImageSelection);
+        Orientation waited = Orientation.ANT_POST;
+        assertEquals(waited, res.getImageOrientation());
     }
 }
