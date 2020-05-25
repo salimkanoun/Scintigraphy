44af9579ead7636cb9ee7b703b4bfb4660d6ae49
Angèle MATEOS <angele_m@orange.fr>
2020-05-25 16:59:12 +0200

    TT ok et fonctionnel

--- src/org/petctviewer/scintigraphy/parathyroid/TabMediastinale.java
+++ src/org/petctviewer/scintigraphy/parathyroid/TabMediastinale.java
@@ -2,21 +2,16 @@ package org.petctviewer.scintigraphy.parathyroid;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import org.petctviewer.scintigraphy.renal.Model_Renal;
-import org.petctviewer.scintigraphy.renal.postMictional.Model_PostMictional;
 import org.petctviewer.scintigraphy.scin.ImagePreparator;
 import org.petctviewer.scintigraphy.scin.ImageSelection;
 import org.petctviewer.scintigraphy.scin.Orientation;
@@ -30,17 +25,12 @@ import org.petctviewer.scintigraphy.scin.gui.FenSelectionDicom.Column;
 import org.petctviewer.scintigraphy.scin.gui.TabContrastModifier;
 import org.petctviewer.scintigraphy.scin.library.Library_Dicom;
 import org.petctviewer.scintigraphy.scin.library.Library_Gui;
-import org.petctviewer.scintigraphy.scin.library.Library_Quantif;
-import org.petctviewer.scintigraphy.scin.preferences.PrefTabRenal;
 
 import ij.ImagePlus;
-import ij.Prefs;
-import ij.util.DicomTools;
 
 
 public class TabMediastinale extends TabContrastModifier implements ActionListener {
     
-    private final boolean bladder;
 	private JButton btn_addImp;
 	private boolean imgSelected;
 	private ImageSelection[] images;
@@ -49,41 +39,11 @@ public class TabMediastinale extends TabContrastModifier implements ActionListen
 
 	public TabMediastinale(FenResults parent) {
 		super(parent, "Mediastinale");
-		this.bladder = Prefs.get(PrefTabRenal.PREF_BLADDER, true);
 		this.imgSelected = false;
 
 		this.reloadDisplay();
 	}
 
-
-	private void inflatePanelExcr(Double rg, Double rd) {
-		Model_Renal modele = (Model_Renal) this.parent.getModel();
-		Double[][] excr = modele.getExcrPM(rg, rd);
-
-		// elements du tableau
-		JLabel[] lbls = new JLabel[]{new JLabel("L"), new JLabel("R"), new JLabel("Max"), new JLabel(
-				"" + naIfNull(excr[0][0])), new JLabel("" + naIfNull(excr[1][0])), new JLabel(
-				"" + Library_Quantif.round(modele.getAdjustedValues().get("lasilix") - 1, 1) + " min"), new JLabel(
-				"" + naIfNull(excr[0][1])), new JLabel("" + naIfNull(excr[1][1])),};
-
-		// panel excr
-		this.panel_excr.removeAll();
-		this.panel_excr.setLayout(new GridLayout(3, 3, 0, 3));
-		this.panel_excr.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1));
-		this.panel_excr.add(new JLabel("Excretion ratio Post-Mict"));
-		// on centre les contenu du tableau
-		for (JLabel l : lbls) {
-			l.setHorizontalAlignment(JLabel.CENTER);
-			this.panel_excr.add(l);
-		}
-	}
-
-	private String naIfNull(Double d) {
-		if (d == null) {
-			return "N/A";
-		}
-		return d + " %";
-	}
 
 	@Override
 	public Component getSidePanelContent() {
@@ -164,20 +124,15 @@ public class TabMediastinale extends TabContrastModifier implements ActionListen
 
 					selectedImages.get(0).close();
 
-					TabMediastinale.this.imgSelected = true;
-					
-//					IJ.run(selections.get(0).getImagePlus(), "Size...",
-//							   "width=" + 512 + " height=" + 512 + " depth=2 constrain average " +
-//									   "interpolation=Bilinear");
-					
-					
+                    TabMediastinale.this.imgSelected = true;
+                    
 					ImagePlus temp = Library_Dicom.resize(selections.get(0).getImagePlus(), 512, 512);
 					selections.get(0).getImagePlus().close();
 					selections.get(0).setImagePlus(temp);
 					Library_Gui.setCustomLut(selections.get(0).getImagePlus());
 					
 					Library_Gui.initOverlay(selections.get(0).getImagePlus());
-					Library_Gui.setOverlayTitle("Post", selections.get(0).getImagePlus(), Color.YELLOW, 1);
+					Library_Gui.setOverlayTitle("Ant", selections.get(0).getImagePlus(), Color.YELLOW, 1);
 					Library_Gui.setOverlayGD(selections.get(0).getImagePlus(), Color.YELLOW);
 					TabMediastinale.this.setImage(selections.get(0).getImagePlus());
 
@@ -197,43 +152,6 @@ public class TabMediastinale extends TabContrastModifier implements ActionListen
 			fen.setVisible(true);
 
 		}
-	}
-
-	public JPanel updateResultFrame(Model_PostMictional model) {
-		Model_Renal modele = (Model_Renal) this.parent.getModel();
-
-		HashMap<String, Double> data = model.getData();
-
-		Double rg = null, rd = null;
-		int duration = Integer.parseInt(DicomTools.getTag(this.getAssociatedImage(), "0018,1242").trim());
-		if (modele.getKidneys()[0]) {
-			rg = data.get("L. Kidney") - data.get("L. bkg");
-			// on calcule les valeurs en coups/sec
-			rg /= (duration / 1000); // TODO: rg is double but (duration / 1000) is int??????
-		}
-		if (modele.getKidneys()[1]) {
-			rd = data.get("R. Kidney") - data.get("R. bkg");
-			// on calcule les valeurs en coups/sec
-			rd /= (duration / 1000); // TODO: rg is double but (duration / 1000) is int??????
-		}
-
-		// creation du panel excr rein gauche et droit
-		this.inflatePanelExcr(rg, rd);
-
-		// ajout de la vessie dans la liste d'organes si elle est selectionnee
-		if (bladder) {
-			Double bld = data.get("Bladder");
-			bld /= (duration / 1000); // TODO: rg is double but (duration / 1000) is int??????
-			this.panel_bladder.add(
-					new JLabel("Bladder : " + Library_Quantif.round(modele.getExcrBladder(bld), 2) + " %"));
-		}
-
-		JPanel flow = new JPanel(new GridLayout(3, 1));
-		flow.add(panel_excr);
-		// Equivalent to a \n
-		flow.add(new JLabel(""));
-		flow.add(super.getSidePanelContent());
-		return flow;
 	}
 
 
