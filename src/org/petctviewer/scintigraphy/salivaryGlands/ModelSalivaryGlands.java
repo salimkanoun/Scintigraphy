e512e6acefc2f7f92e3596c159129d2e155cd5ae
Axel Metzinger <axelmetzinger@gmail.com>
2021-04-29 16:00:24 +0200

    Add results computation to Salivary glands

--- src/org/petctviewer/scintigraphy/salivaryGlands/ModelSalivaryGlands.java
+++ src/org/petctviewer/scintigraphy/salivaryGlands/ModelSalivaryGlands.java
@@ -1,10 +1,12 @@
 package org.petctviewer.scintigraphy.salivaryGlands;
 
 import ij.ImagePlus;
+import ij.Prefs;
 import ij.gui.Roi;
 import ij.util.DicomTools;
 import org.jfree.data.xy.XYSeries;
 import org.petctviewer.scintigraphy.scin.ImageSelection;
+import org.petctviewer.scintigraphy.scin.library.Library_Dicom;
 import org.petctviewer.scintigraphy.scin.library.Library_JFreeChart;
 import org.petctviewer.scintigraphy.scin.library.Library_Quantif;
 import org.petctviewer.scintigraphy.scin.model.ModelScinDyn;
@@ -12,6 +14,7 @@ import org.petctviewer.scintigraphy.scin.model.ModelScinDyn;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ModelSalivaryGlands extends ModelScinDyn {
 
@@ -40,42 +43,39 @@ public class ModelSalivaryGlands extends ModelScinDyn {
        this.pixelCounts = new HashMap<>();
    }
 
+    @SuppressWarnings("rawtypes")
+    public HashMap<Comparable, Double> getAdjustedValues() {
+        return this.adjustedValues;
+    }
+
    @SuppressWarnings("rawtypes")
    public void setAdjustedValues(HashMap<Comparable, Double> hashMap) {
        this.adjustedValues = hashMap;
    }
 
-   /**
-    * renvoie le temps a tmax et t1/2
-    * 
-    * @return res[0][x] : tMax, res[1][x] : t1/2, res[x][0] : rein gauche,
-    *         res[x][1] : rein droit
-    */
-   public Double[][] getTiming() {
-       Double[][] res = new Double[2][2];
+    public ArrayList<String> getGlands() {
+        return glands;
+    }
 
-       if (true) {
-           Double xMaxG = this.adjustedValues.get("tmax L");
-           XYSeries lk = this.getSerie("Final KL");
-           res[1][0] = Library_Quantif.round(Library_JFreeChart.getTDemiObs(lk, xMaxG), 1);
-           res[0][0] = Library_Quantif.round(xMaxG, 2);
-       } else {
-           res[0][0] = Double.NaN;
-           res[1][0] = Double.NaN;
-       }
+    public int[] getFrameDurations() {
+        return frameDurations;
+    }
 
-       if (true) {
-           Double xMaxD = this.adjustedValues.get("tmax R");
-           XYSeries rk = this.getSerie("Final KR");
-           res[1][1] = Library_Quantif.round(Library_JFreeChart.getTDemiObs(rk, xMaxD), 1);
-           res[0][1] = Library_Quantif.round(xMaxD, 2);
-       } else {
-           res[0][1] = Double.NaN;
-           res[1][1] = Double.NaN;
-       }
+    public ImageSelection getImpAnt() {
+        return impAnt;
+    }
 
-       return res;
-   }
+    public int getPixelCount(String roiName) {
+        return this.pixelCounts.get(roiName);
+    }
+
+    public void enregistrerPixelRoi(String roiName, int pixelNumber) {
+        this.pixelCounts.put(roiName, pixelNumber);
+    }
+
+    public HashMap<String, Integer> getPixelRoi() {
+        return this.pixelCounts;
+    }
 
    /**
     * calcule le Excr selon le temps d'injection du lasilix
@@ -111,69 +111,30 @@ public class ModelSalivaryGlands extends ModelScinDyn {
        return res;
    }
 
-   @SuppressWarnings("rawtypes")
-   public HashMap<Comparable, Double> getAdjustedValues() {
-       return this.adjustedValues;
-   }
-
-   public double getExcrBladder(Double bld) {
-       XYSeries bldSeries = this.getSerie("Bladder");
-       return 100 * bld / Library_JFreeChart.getY(bldSeries, bldSeries.getMaxX());
-   }
-
    /**
     * Renvoie la hauteur des reins en cm, index 0 : rein gauche, 1 : rein droit
     */
-   public Double[] getSize() {
-       int heightLK=0;
-       if (this.organRois.containsKey("L. Parotid")) {
-            heightLK = this.organRois.get("L. Parotid").getBounds().height;
-       }
-       int heightRK =0;
-       if (this.organRois.containsKey("R. Parotid")) {
-            heightRK = this.organRois.get("R. Parotid").getBounds().height;
-       }
-       //r�cup�re la hauteur d'un pixel en mm
-       //Calibration calibration=this.getImp().getLocalCalibration();
-       //calibration.setUnit("mm");
-       //Double pixelHeight=calibration.pixelHeight;
-       ///System.out.println(pixelHeight);
+   public Map<String,Double> getSize() {
        String pixelHeightString = DicomTools.getTag(this.getImagePlus(), "0028,0030").trim().split("\\\\")[1];
        double pixelHeight = Double.parseDouble(pixelHeightString);
-       Double[] kidneyHeight = new Double[2];
 
-       // convvertion des pixel en mm
-       kidneyHeight[0] = Library_Quantif.round(heightLK * pixelHeight / 10, 2);
-       kidneyHeight[1] = Library_Quantif.round(heightRK * pixelHeight / 10, 2);
+       Map<String, Double> glandsHeight = new HashMap<>();
 
-       return kidneyHeight;
-   }
-
-   public ArrayList<String> getGlands() {
-       return glands;
-   }
-
-   @SuppressWarnings("rawtypes")
-   private String getDataString(Comparable key, String name) {
-       if(this.getData().containsKey(key)) {
-           StringBuilder nameBuilder = new StringBuilder(name);
-           for(Double d : this.getData().get(key)) {
-               nameBuilder.append(",").append(d);
-           }
-           name = nameBuilder.toString();
+       for (String gland: this.organRois.keySet()) {
+           //get & convert height to mm
+           int height = this.organRois.get(gland).getBounds().height;
+           glandsHeight.put(gland, Library_Quantif.round(height * pixelHeight / 10, 2));
        }
-       name += "\n";
-       return name;
+
+       return glandsHeight;
    }
 
    /********** Public *********/
    public void enregistrerMesure(String nomRoi, ImagePlus imp) {
        if (this.isUnlocked()) {
-
            this.organRois.put(nomRoi, imp.getRoi());
 
            this.getData().computeIfAbsent(nomRoi, k -> new ArrayList<>());
-           
            // on y ajoute le nombre de coups
            this.getData().get(nomRoi).add(Math.max(Library_Quantif.getCounts(imp),1.0d) );
        }
@@ -198,24 +159,50 @@ public class ModelSalivaryGlands extends ModelScinDyn {
        // on soustrait le bruit de fond
        this.substractBkg();
 
+       //time of lemon juice stimulation in minute
+       int lemonInjection = (int) Prefs.get("Lemon Injection",
+               (this.adjustedValues.containsKey("Lemon Injection")) ? this.adjustedValues.get("Lemon Injection") : 10);
+
+       int lemon = ModelScinDyn.getSliceIndexByTime(lemonInjection * 60 * 1000, this.frameDurations);
+       int lemonMinus1 = ModelScinDyn.getSliceIndexByTime((lemonInjection - 1) * 60 * 1000, this.frameDurations);
+       int lemonPlus2 = ModelScinDyn.getSliceIndexByTime((lemonInjection + 2) * 60 * 1000, this.frameDurations);
+       int lemonPlus4 = ModelScinDyn.getSliceIndexByTime((lemonInjection + 4) * 60 * 1000, this.frameDurations);
+       int lemonPlus10 = ModelScinDyn.getSliceIndexByTime((lemonInjection + 10) * 60 * 1000, this.frameDurations);
+
        this.uptakeRatio = new HashMap<>();
        this.excretionFraction = new HashMap<>();
        this.impAnt.getImagePlus().setRoi(this.organRois.get("Background"));
        //the average counts in the ipsilateral background reference between 10 and 20 min
-       double avgBck = Library_Quantif.getAvgCounts(this.impAnt.getImagePlus());
+       ImagePlus bkg10_20 = Library_Dicom.project(this.impAnt, lemon, lemonPlus10, "avg").getImagePlus();
+       double avgBck = Library_Quantif.getCounts(bkg10_20) / Library_Quantif.getPixelNumber(bkg10_20);
 
        for (String s: this.getGlands()) {
-           double max = 4;  //the highest count rate in the ninth or 10th minute
-           double min = 1;  //the lowest count rate 2–4 min after lemon juice stimulation
-           double ur = max/avgBck;
+           this.impAnt.getImagePlus().setRoi(this.organRois.get(s));
+
+           //the highest count rate in the ninth or 10th minute
+           double max = 0, prec = 0;
+           ImagePlus impMax = null;
+           for (int i = lemonPlus2; i <= lemonPlus4; i++) {
+               this.impAnt.getImagePlus().setPosition(i);
+               max = Math.max(max, Library_Quantif.getCounts(this.impAnt.getImagePlus()));
+               if (prec != max)
+                   impMax = this.impAnt.getImagePlus();
+           }
+
+           //the lowest count rate 2–4 min after lemon juice stimulation
+           double min = Double.MAX_VALUE;
+           for (int i = lemonMinus1; i <= lemon; i++) {
+               this.impAnt.getImagePlus().setPosition(i);
+               min = Math.min(min, Library_Quantif.getCounts(this.impAnt.getImagePlus()));
+           }
+           double ur = max/(avgBck * Library_Quantif.getPixelNumber(impMax));
            double ef = 1 - min/max;
 
            this.uptakeRatio.put(s, ur);
            this.excretionFraction.put(s, ef);
        }
 
-       System.out.println(this.uptakeRatio.toString());
-       System.out.println(this.excretionFraction.toString());
+       System.out.println(this);
 
        // ***INTEGRALE DE LA COURBE VASCULAIRE***
        // on recupere la liste des donnees vasculaires
@@ -263,38 +250,42 @@ public class ModelSalivaryGlands extends ModelScinDyn {
     */
    @Override
    public String toString() {
-       Double[][] excr = this.getExcr();
-       Double[][] timing = this.getTiming();
-
        StringBuilder s = new StringBuilder(super.toString());
        
        s.append("\n");
-       
        s.append(getDataString("Final L. Parotid", "Corrected Left Parotid"));
        s.append(getDataString("Final R. Parotid", "Corrected Right Parotid"));
-       s.append(getDataString("Final L. Parotid", "Corrected Left Submandible"));
-       s.append(getDataString("Final R. Parotid", "Corrected Right Submandible"));
+       s.append(getDataString("Final L. SubMandib", "Corrected Left Submandible"));
+       s.append(getDataString("Final R. SubMandib", "Corrected Right Submandible"));
+       s.append("\n");
 
-       s.append("\n");
-       s.append(",time, left kidney, right kidney \n");
-       
-       s.append("Timing tmax , ,").append(timing[0][0]).append(",").append(timing[0][1]).append("\n");
-       s.append("Timing t1/2 , ,").append(timing[1][0]).append(",").append(timing[1][1]).append("\n");
-       
-       s.append("\n");
-       
-       
-       
-       // ROE
-       Double xLasilix = this.adjustedValues.get("lasilix");
-       Double[] time = {Library_Quantif.round(xLasilix - 1, 1),
-               Library_Quantif.round(xLasilix + 2, 1), 
-               Library_Quantif.round(this.getSerie("Blood Pool").getMaxX(), 1)};
-       
-       s.append(s.append(super.toString()));
+       StringBuilder name = new StringBuilder();
+       StringBuilder ur = new StringBuilder("Uptake ratio");
+       StringBuilder ef = new StringBuilder("Excretion fraction");
+       for (String gland: this.getGlands()) {
+           name.append(",").append(gland);
+           ur.append(",").append(this.uptakeRatio.get(gland));
+           ef.append(",").append(this.excretionFraction.get(gland));
+       }
+       s.append(name)
+               .append("\n").append(ur)
+               .append("\n").append(ef);
+
        return s.toString();
-
    }
+
+    @SuppressWarnings("rawtypes")
+    private String getDataString(Comparable key, String name) {
+        StringBuilder nameBuilder = new StringBuilder(name);
+
+        if(this.getData().containsKey(key)) {
+            for(Double d : this.getData().get(key)) {
+                nameBuilder.append(",").append(d);
+            }
+            nameBuilder.append("\n");
+        }
+        return nameBuilder.toString();
+    }
 
    private void substractBkg() {
        int aireBkg = this.organRois.get("Background").getStatistics().pixelCount;
@@ -324,26 +315,4 @@ public class ModelSalivaryGlands extends ModelScinDyn {
        }
 
    }
-   
-   public int[] getFrameDurations() {
-       return frameDurations;
-   }
-   
-   public ImageSelection getImpAnt() {
-       return impAnt;
-   }
-
-   public void enregistrerPixelRoi(String roiName, int pixelNumber) {
-       this.pixelCounts.put(roiName, pixelNumber);
-   }
-   
-   public int getPixelCount(String roiName) {
-       return this.pixelCounts.get(roiName);
-   }
-   
-   public HashMap<String, Integer> getPixelRoi() {
-       return this.pixelCounts;
-   }
-
-   
 }
