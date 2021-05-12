936ac4e26d524e93732a753761a3a17ec6bd12cb
Lou-Anne <louanne.costes@gmail.com>
2021-05-12 16:37:26 +0200

    scintivol réparé + détails

--- src/org/petctviewer/scintigraphy/hepatic/scintivol/Model_Scintivol.java
+++ src/org/petctviewer/scintigraphy/hepatic/scintivol/Model_Scintivol.java
@@ -23,6 +23,7 @@ public class Model_Scintivol extends ModelScinDyn {
     private double size;
     private double weight;
     private final ArrayList<String> organes;
+    private int[] frameDurations;
 
 
     public Model_Scintivol(ImageSelection[] selectedImages, String studyName, int[] frameDuration) {
@@ -178,6 +179,8 @@ public class Model_Scintivol extends ModelScinDyn {
     @Override
     public void calculateResults() {
         int[] frameDurations = Library_Dicom.buildFrameDurations(this.getImagePlus());
+
+        this.setFrameduration(frameDurations);
         double tracerDelayTime = this.getTracerDelayTime();
         int sliceT1 = getSliceIndexByTime((tracerDelayTime + 150) * 1000, frameDurations);
         int sliceT2 = getSliceIndexByTime((tracerDelayTime + 350) * 1000, frameDurations);
